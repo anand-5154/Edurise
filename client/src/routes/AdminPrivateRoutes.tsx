339b00223b01c69a6b4338be03fb3a5afe3f66c3
anand <anand.krishna5154@gmail.com>
2025-11-20 08:26:38 +0530

    merge with main branch

--- client/src/routes/AdminPrivateRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-const AdminPrivateRoute = () => {
-  const isAdminLoggedIn = localStorage.getItem("adminToken") !== null
-
-  return isAdminLoggedIn ? <Outlet /> : <Navigate to="/admin/login" />
-}
-
-export default AdminPrivateRoute
--- client/src/types/category.types.ts
+++
@@ -1,9 +0,0 @@
-export interface Category{
-    _id:string
-    name:string,
-    isDeleted:boolean
-}
-
-export interface CategoryResponse {
-  message: string;
-}
